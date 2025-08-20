7cc11655588d073471e7cb10e9b8056f686e662e
nabin <sharmanabin.0910@gmail.com>
2025-08-20 14:15:10 +0530

    Double and boolean in scanner class

--- UsingScanners.java
+++ UsingScanners.java
@@ -10,8 +10,19 @@ public class UsingScanners {
         System.out.print("Enter your age : ");
         int age = scanner.nextInt();
 
+        System.out.print("Enter your GPA : ");
+        double gpa = scanner.nextDouble();
+
+        System.out.print("Are you a student? (true/false) : ");
+        boolean stud = scanner.nextBoolean();
+
+
         System.out.println("hello , "+ name);
         System.out.println("you are "+age+" years old");
+        System.out.println("Your GPA is : "+gpa);
+        if(stud) System.out.println("You are a student !");
+        else System.out.println("You are not a student !");
+
 
     }
 }
